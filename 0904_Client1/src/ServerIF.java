cacb1c7908d111fa18bd1cf5a103a2c50a7201fc
9sjymnk <90037748+9sjymnk@users.noreply.github.com>
2024-10-01 00:46:06 +0900

    Delete 0904_Client1/src directory

--- 0904_Client1/src/ServerIF.java
+++
@@ -1,8 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface ServerIF extends Remote {
-
-    void saveName(String name) throws RemoteException;
-    String getName() throws RemoteException;
-}
